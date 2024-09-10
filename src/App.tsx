4cda64762e0f8443418efb27b5803d95596b8417
thbueno <thiagobueno.s@gmail.com>
2024-09-10 23:50:05 +0700

    First page descriptions

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,13 @@
 export function App() {
   return (
-    <h1 className="text-3xl font-bold underline">
-      Good Morning Vietnam!
-    </h1>
+    <div className="h-screen">
+    <div className="max-w-3xl px-6">
+      <p>Convide seus amigos e planeje sua proxima viagem!</p>
+      <div>
+
+      </div>
+      <p>Ao planejar sua viagem pela Travel with Friends você automaticamente concorda com os nossos <a href="#">termos de uso</a> e <a href="#">politicas de privacidade</a>.</p>    
+    </div>
+    </div>
   )
 }

cc27be5c9f14cdb89e42301ad4a8e70df2018425
thbueno <thiagobueno.s@gmail.com>
2024-10-25 11:48:23 +0700

    html a initial style on app

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,12 @@
 export function App() {
   return (
-    <div className="h-screen">
-    <div className="max-w-3xl px-6">
-      <p>Convide seus amigos e planeje sua proxima viagem!</p>
+    <div className="h-screen flex items-center justify-center">
+    <div className="max-w-3xl px-6 text-center">
+      <p className="text-zinc-300 text-lg">Convide seus amigos e planeje sua proxima viagem!</p>
       <div>
 
       </div>
-      <p>Ao planejar sua viagem pela Travel with Friends você automaticamente concorda com os nossos <a href="#">termos de uso</a> e <a href="#">politicas de privacidade</a>.</p>    
+      <p className="text-zinc-500 text-sm">Ao planejar sua viagem pela Travel with Friends você automaticamente concorda < br />  com os nossos <a className="text-zinc-300 underline" href="#">termos de uso</a> e <a className="text-zinc-300 underline" href="#">politicas de privacidade</a>.</p>    
     </div>
     </div>
   )

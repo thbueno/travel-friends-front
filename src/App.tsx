883d2e6280bdb0f41f8c4c4e89a7faa9829db536
thbueno <thiagobueno.s@gmail.com>
2024-11-06 10:41:23 +0700

    isGuestsInputOpen use state logic

--- src/App.tsx
+++ src/App.tsx
@@ -1,27 +1,48 @@
-import { Calendar, MapPin } from "lucide-react"
+import { ArrowRight, Calendar, MapPin } from "lucide-react"
+import { useState } from "react"
 export function App() {
-  return (
-    <div className="h-screen flex items-center justify-center">
-    <div className="max-w-3xl w-full px-6 text-center space-y-10">
-      <p className="text-zinc-300 text-lg">Convide seus Amigos e planeje sua proxima viagem!</p>
- 
-      <div className="h-16 bg-zinc-900 px-4 rounded-xl flex items-center shadow-shape">
+  const [isGuestsInputOpen, setIsGuestsInputOpen] = useState(false)
 
-        <div className="flex items-center gap-2">
+  function openGuestsInput() {
+    setIsGuestsInputOpen(true)
+  }
+
+
+
+
+  return (
+    <div className="h-screen flex items-center justify-center bg-pattern bg-no-repeat bg-center">
+    <div className="max-w-3xl w-full px-6 text-center space-y-10">
+
+      <div className="flex flex-col items-center gap-3">
+        <img src="/logo.svg" alt="TravelFriends" className="mx-auto" />
+      <p className="text-zinc-300 text-lg">Convide seus Amigos e planeje sua proxima viagem!</p>
+      </div>
+ 
+      <div className="h-16 bg-zinc-900 px-4 rounded-xl flex items-center shadow-shape gap-3">
+
+        <div className="flex items-center gap-2 flex-1">
         <MapPin className="size-5 text-zinc-400" />
-        <input type="text" placeholder="Para onde você vai?" className="bg-transparent text-lg placeholder-zinc-400" />
+        <input type="text" placeholder="Para onde você vai?" className="bg-transparent text-lg placeholder-zinc-400 outline-none flex-1" />
         </div>
 
         
         <div className="flex items-center gap-2">
         <Calendar className="size-5 text-zinc-400" />
-        <input type="text" placeholder="Quando?" className="bg-transparent text-lg placeholder-zinc-400" />
+        <input type="text" placeholder="Quando?" className="bg-transparent text-lg placeholder-zinc-400 w-40 outline-none" />
         </div>
 
-        <button>Cotinuar</button>
+        <div className="w-px h-6 bg-zinc-800" />
+
+        <button onClick={openGuestsInput} className="flex items-center gap-2 bg-lime-300 text-lime-950 rounded-lg px-5 py-2 font-medium hover:bg-lime-400">
+          Continuar
+          <ArrowRight className="size-5" />
+        </button>
 
 
       </div>
+
+      {isGuestsInputOpen ? "aberto" : "fechado"}
 
       <p className="text-zinc-500 text-sm">Ao planejar sua viagem pela Travel with Friends você automaticamente concorda < br />  com os nossos <a className="text-zinc-300 underline" href="#">termos de uso</a> e <a className="text-zinc-300 underline" href="#">politicas de privacidade</a>.</p>    
     </div>

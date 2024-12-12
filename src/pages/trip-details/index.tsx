d0926b0e8ed06f2913594453066788c92bfedd99
thbueno <thiagobueno.s@gmail.com>
2024-12-12 17:59:46 +0700

    Finish sidebar and create Modal registrar atividade

--- src/pages/trip-details/index.tsx
+++ src/pages/trip-details/index.tsx
@@ -1,7 +1,30 @@
-import { Calendar, CircleCheck, MapPin, Plus, Settings2 } from 'lucide-react';
+import {
+	Calendar,
+	CircleCheck,
+	CircleDashed,
+	Link2,
+	MapPin,
+	Plus,
+	Settings2,
+	Tag,
+	UserCog,
+	X,
+} from 'lucide-react';
+import { useState } from 'react';
 
 /* eslint-disable react/react-in-jsx-scope */
 export function TripDetailsPage() {
+	const [isCreateActivityModalOpen, setIsCreateActivityModalOpen] =
+		useState(false);
+
+	function openCreateActivityModal() {
+		setIsCreateActivityModalOpen(true);
+	}
+
+	function closeCreateActivityModal() {
+		setIsCreateActivityModalOpen(false);
+	}
+
 	return (
 		<div className="max-w-6xl px-6 py-10 mx-auto space-y-8">
 			<div className="flex items-center justify-between  px-4 h-16 rounded-xl bg-zinc-900 shadow-shape ">
@@ -38,7 +61,10 @@ export function TripDetailsPage() {
 					{/* Atividades e Cadastrar atividade */}
 					<div className=" flex items-center justify-between">
 						<h2 className="text-3xl font-semibold">Atividades</h2>
-						<button className="flex items-center gap-2 bg-lime-300 text-lime-950 rounded-lg px-5 py-2 font-medium hover:bg-lime-400">
+						<button
+							onClick={openCreateActivityModal}
+							className="flex items-center gap-2 bg-lime-300 text-lime-950 rounded-lg px-5 py-2 font-medium hover:bg-lime-400"
+						>
 							<Plus className="size-5" />
 							Cadastrar atividade
 						</button>
@@ -86,18 +112,144 @@ export function TripDetailsPage() {
 
 				{/* Sidebar  */}
 				<div className="w-80 space-y-6">
+					{/* Links Importantes */}
 					<div className="space-y-6">
 						<h2 className="font-semibold text-xl">Links importantes</h2>
+						<div className="space-y-5">
+							{/* Link 1 */}
+							<div className="flex items-center justify-between">
+								<div className="space-y-1.5">
+									<span className="block font-medium text-zinc-100">
+										Reserva do AirBnB
+									</span>
+									<a
+										href="#"
+										className="block text-zinc-400 hover:text-zinc-200 text-xs truncate"
+									>
+										https://airbnb.com/rooms/10470039003909348084083048208042
+									</a>
+								</div>
+								<Link2 className="text-zinc-400 size-5 shrink-0" />
+							</div>
 
-						<button className="flex items-center justify-center w-full bg-zinc-800 text-zinc-200 gap-2 bg-l rounded-lg px-5 py-2 font-medium hover:bg-zinc-700">
+							{/* Link 2 */}
+							<div className="flex items-center justify-between">
+								<div className="space-y-1.5">
+									<span className="block font-medium text-zinc-100">
+										Reserva do AirBnB
+									</span>
+									<a
+										href="#"
+										className="block text-zinc-400 hover:text-zinc-200 text-xs truncate"
+									>
+										https://airbnb.com/rooms/10470039003909348084083048208042
+									</a>
+								</div>
+								<Link2 className="text-zinc-400 size-5 shrink-0" />
+							</div>
+						</div>
+
+						<button className="flex items-center justify-center w-full bg-zinc-800 text-zinc-200 gap-2 bg-l rounded-lg px-5 h-11 font-medium hover:bg-zinc-700">
 							<Plus className="size-5" />
 							Cadastrar Novo Link
 						</button>
 					</div>
+
 					<div className="w-full h-px bg-zinc-800" />
+
+					{/* Convidados */}
+
+					<div className="space-y-6">
+						<h2 className="font-semibold text-xl">Convidados</h2>
+
+						<div className="space-y-5">
+							{/* Convidado 1 */}
+
+							<div className="flex items-center justify-between">
+								<div className="space-y-1.5">
+									<span className="block font-medium text-zinc-100">
+										Edino Bert
+									</span>
+									<span className="block text-zinc-400 text-sm truncate">
+										edinobert@me.com
+									</span>
+								</div>
+								<CircleDashed className="text-zinc-400 size-5 shrink-0" />
+							</div>
+
+							{/* Convidado 2 */}
+
+							<div className="flex items-center justify-between">
+								<div className="space-y-1.5">
+									<span className="block font-medium text-zinc-100">
+										Edino Bert
+									</span>
+									<span className="block text-zinc-400 text-sm truncate">
+										edinobert@me.com
+									</span>
+								</div>
+								<CircleDashed className="text-zinc-400 size-5 shrink-0" />
+							</div>
+						</div>
+
+						<button className="flex items-center justify-center w-full bg-zinc-800 text-zinc-200 gap-2 bg-l rounded-lg px-5 h-11 font-medium hover:bg-zinc-700">
+							<UserCog className="size-5" />
+							Gerenciar Convidados
+						</button>
+					</div>
+
 					<div></div>
 				</div>
 			</main>
+
+			{isCreateActivityModalOpen && (
+				<div className="fixed inset-0 bg-black/60 flex items-center justify-center">
+					<div className="w-[648px] rounded-lg py-5 px-6 shadow-shape bg-zinc-900 space-y-5">
+						{/* TITLE AND DESCRIPTION  */}
+						<div className="space-y-2">
+							<div className="flex items-center justify-between">
+								<h2 className="text-lg font-semibold">Cadastrar Atividade</h2>
+								<button type="button" onClick={closeCreateActivityModal}>
+									<X className="size-5 text-zinc-400" />
+								</button>
+							</div>
+
+							<p className="text-zinc-400 text-sm">
+								Todos os convidados podem ver as atividades.
+							</p>
+						</div>
+
+						{/* FORM MODAL */}
+						<form className="space-y-3">
+							<div className="flex items-center gap-2 py-2.5 px-4 bg-zinc-950 border border-zinc-800 rounded-lg">
+								<Tag className="text-zinc-400 size-5" />
+								<input
+									name="title"
+									placeholder="Qual a atividade?"
+									className="bg-transparent text-lg placeholder-zinc-400 outline-none flex-1"
+								/>
+							</div>
+
+							<div className="flex-1 flex items-center gap-2 h-14 py-2.5 px-4 bg-zinc-950 border border-zinc-800 rounded-lg">
+								<Calendar className="text-zinc-400 size-5" />
+								<input
+									type="datetime-local"
+									name="occurs_at"
+									placeholder="Data e horário da atividade"
+									className="bg-transparent text-lg placeholder-zinc-400 outline-none flex-1"
+								/>
+							</div>
+
+							<button
+								type="submit"
+								className="flex items-center justify-center w-full gap-2 bg-lime-300 text-lime-950 rounded-lg px-5 py-2 font-medium hover:bg-lime-400"
+							>
+								Salvar atividade
+							</button>
+						</form>
+					</div>
+				</div>
+			)}
 		</div>
 	);
 }
